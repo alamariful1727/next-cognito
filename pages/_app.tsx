21a862d6d3e1e3fc6d52ecd905c6008a9d0d6334
Ariful Alam <alamariful1727@gmail.com>
2021-06-28 19:44:37 +0600

    tailwindcss added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
 import Layout from '../components/Layout';
+import './../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 export default MyApp;
