01f50e30b2f14263946ae5512f637bc1c3ed63b3
Ariful Alam <alamariful1727@gmail.com>
2021-06-28 13:37:35 +0600

    custom _document added

--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,10 @@
-export const App_Title = 'Next App';
+export const App_Title = 'Next Cognito';
+export const App_Description = 'Next.js (TYPESCRIPT) with AWS Cognito.';
+export const Social = {
+	linkedin: 'https://www.linkedin.com/in/alamariful1727',
+	github: 'https://github.com/alamariful1727',
+	facebook: 'https://www.facebook.com/alamariful1727',
+	twitter: 'https://twitter.com/alamariful1727',
+};
+export const Profile_Image_URL =
+	'https://media-exp1.licdn.com/dms/image/C5103AQHt5Y4HxV3bRg/profile-displayphoto-shrink_200_200/0/1540822950782?e=1630540800&v=beta&t=PJgEjJxThgXtBAPNnU62Ufx9AgLwgaWLAFbEJVTVIag';
