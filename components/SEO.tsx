7d9a387cd0b1cf4b98c5bbdae3076c76b628ac64
Ariful Alam <alamariful1727@gmail.com>
2021-06-29 20:17:55 +0600

    SEO updated

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -14,13 +14,13 @@ export const SEO = ({ siteTitle, description, url, image }: props) => {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       {/* <!-- Primary Meta Tags --> */}
       <title>{siteTitle ? `${App_Title} | ${siteTitle}` : App_Title}</title>
-      <meta name="description" content={App_Description || description} />
+      <meta name="description" content={description || App_Description} />
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={App_Title} />
       <meta property="og:site_name" content={siteTitle} />
-      <meta property="og:description" content={App_Description || description} />
+      <meta property="og:description" content={description || App_Description} />
       <meta property="og:url" content={url || Social.linkedin} />
       <meta property="og:image" content={image || Profile_Image_URL} />
 
@@ -28,7 +28,7 @@ export const SEO = ({ siteTitle, description, url, image }: props) => {
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:creator" content={Social.twitter} />
       <meta property="twitter:title" content={siteTitle ? `${App_Title} | ${siteTitle}` : App_Title} />
-      <meta property="twitter:description" content={App_Description || description} />
+      <meta property="twitter:description" content={description || App_Description} />
       <meta property="twitter:url" content={url || Social.linkedin} />
       <meta property="twitter:image" content={image || Profile_Image_URL} />
     </Head>
