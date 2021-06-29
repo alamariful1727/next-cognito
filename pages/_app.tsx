fc1e54faee1951134f1a36ab54f9e3dafe71523a
Ariful Alam <alamariful1727@gmail.com>
2021-06-30 00:42:18 +0600

    react-query and axios added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,28 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
+import NextNprogress from 'nextjs-progressbar';
 import Layout from '../components/Layout';
-import 'react-toastify/dist/ReactToastify.css';
-import './../styles/tailwind.css';
 import { ThemeProvider } from '@material-ui/core';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { theme } from '../config/mui';
 import '../config/amplify';
-import NextNprogress from 'nextjs-progressbar';
+import 'react-toastify/dist/ReactToastify.css';
+import './../styles/tailwind.css';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <NextNprogress color="#4BE193" startPosition={0.1} stopDelayMs={200} height={3} showOnShallow={true} />
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <NextNprogress color="#4BE193" startPosition={0.1} stopDelayMs={200} height={3} showOnShallow={true} />
+          <Component {...pageProps} />
+        </Layout>
+        {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 export default MyApp;
