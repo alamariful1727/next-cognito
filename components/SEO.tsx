66af6789031f7b43fe019bb4cd6a7117cb28cf2e
Ariful Alam <alamariful1727@gmail.com>
2021-06-30 23:10:44 +0600

    New rows added in Browse page and SEO title updated

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -13,12 +13,12 @@ export const SEO = ({ siteTitle, description, url, image }: props) => {
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       {/* <!-- Primary Meta Tags --> */}
-      <title>{siteTitle ? `${App_Title} | ${siteTitle}` : App_Title}</title>
+      <title>{siteTitle ? `${siteTitle} | ${App_Title}` : App_Title}</title>
       <meta name="description" content={description || App_Description} />
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={siteTitle ? `${App_Title} | ${siteTitle}` : App_Title} />
+      <meta property="og:title" content={siteTitle ? `${siteTitle} | ${App_Title}` : App_Title} />
       <meta property="og:site_name" content={siteTitle} />
       <meta property="og:description" content={description || App_Description} />
       <meta property="og:url" content={url || Social.linkedin} />
@@ -27,7 +27,7 @@ export const SEO = ({ siteTitle, description, url, image }: props) => {
       {/* <!-- Twitter --> */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:creator" content={Social.twitter} />
-      <meta property="twitter:title" content={siteTitle ? `${App_Title} | ${siteTitle}` : App_Title} />
+      <meta property="twitter:title" content={siteTitle ? `${siteTitle} | ${App_Title}` : App_Title} />
       <meta property="twitter:description" content={description || App_Description} />
       <meta property="twitter:url" content={url || Social.linkedin} />
       <meta property="twitter:image" content={image || Profile_Image_URL} />
--- pages/browse.tsx
+++ pages/browse.tsx
@@ -7,6 +7,8 @@ const Browse = () => {
       <SEO siteTitle="Browse" description="Browse movies & series here." />
       <h1 className="text-3xl text-gray-800 font-semibold underline">Browse movies & series by category!!</h1>
       <div className="space-y-8">
+        <MoviesRowSlider title="Comedy" categoryName="comedy" />
+        <MoviesRowSlider title="Comedy Series" categoryName="comedy" showSeason={true} />
         <MoviesRowSlider title="Controversial" categoryName="controversial" />
         <MoviesRowSlider title="Controversial Series" categoryName="controversial" showSeason={true} />
         <MoviesRowSlider title="Documentaries" categoryName="documentaries" />
@@ -17,6 +19,8 @@ const Browse = () => {
         <MoviesRowSlider title="Educational Series" categoryName="educational" showSeason={true} />
         <MoviesRowSlider title="Episodic" categoryName="episodic" />
         <MoviesRowSlider title="Episodic Series" categoryName="episodic" showSeason={true} />
+        <MoviesRowSlider title="Horror" categoryName="horror" />
+        <MoviesRowSlider title="Horror Series" categoryName="horror" showSeason={true} />
         <MoviesRowSlider title="Music Video" categoryName="musicVideo" />
         <MoviesRowSlider title="Music Video Series" categoryName="musicVideo" showSeason={true} />
         <MoviesRowSlider title="Reality" categoryName="reality" />
@@ -25,6 +29,8 @@ const Browse = () => {
         <MoviesRowSlider title="Shorts Series" categoryName="shorts" showSeason={true} />
         <MoviesRowSlider title="Student" categoryName="student" />
         <MoviesRowSlider title="Student Series" categoryName="student" showSeason={true} />
+        <MoviesRowSlider title="Women In Film" categoryName="womenInFilm" />
+        <MoviesRowSlider title="Women In Film Series" categoryName="womenInFilm" showSeason={true} />
       </div>
     </div>
   );
